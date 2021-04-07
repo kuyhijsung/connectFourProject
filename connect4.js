3d5c49f0dbd375ce035dc5ddda5d4d24e76177c7
Andy <jihyuk.sung@gmail.com>
2021-04-07 09:16:35 -0600

    Finished

--- connect4.js
+++ connect4.js
@@ -114,9 +114,9 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-  // if (board.every(row => row.every(cell => cell))) {
-  //   return endGame('Tie!');
-  // }
+  if (board.every(row => row.every(cell => cell))) {
+    return endGame('Tie!');
+  }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
